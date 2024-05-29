64ec077e6385671852e7bcf4db9b89f56bd355b7
annieherieau <annie.herieau@gmail.com>
2024-05-29 17:20:17 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 // Kata written by Matthieu BRAULT for the next-react formation from TheHackingProject
-import React, { Component } from "react";
-// import { Avatar, Button, Card, Col, Icon, Row } from 'antd/es';
-// import MentionsTagsComponent from './MentionsTagsComponent';
-import PostsList from "./components/PostsList";
-import Profile from "./components/Profile";
+import React from "react";
 import profileData from "./data/profileData";
+import Profile from "./components/Profile";
+import PostsList from "./components/PostsList";
 
-
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-    };
-  
-
-  }
-
-  render() {
-    return (
-      <div style={{ margin: 50 }}>
-        <Profile profileInfos={profileData} />
-        <PostsList posts={profileData.posts} />
-      </div>
-    );
-  }
+export default function App() {
+  const profileInfos = {...profileData};
+  delete profileInfos.posts;
+  return (
+    <div style={{ margin: 50 }}>
+      <Profile profileInfos={profileInfos} />
+      <PostsList posts={profileData.posts} />
+    </div>
+  );
 }
-
-export default App;
